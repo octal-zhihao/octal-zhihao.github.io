694d4390309471cae76051c05d8409d148499614
octal-zhihao <1187456954@qq.com>
2024-09-02 20:34:20 +0800

    Site updated: 2024-09-02 20:34:19

--- js/readPercent.js
+++ js/readPercent.js
@@ -9,7 +9,7 @@ function percent() {
     if (result <= 95) {
         up.childNodes[0].style.display = 'none'
         up.childNodes[1].style.display = 'block'
-        up.childNodes[1].innerHTML = result;
+        up.childNodes[1].innerHTML = result + "%"
     } else {
         up.childNodes[1].style.display = 'none'
         up.childNodes[0].style.display = 'block'
